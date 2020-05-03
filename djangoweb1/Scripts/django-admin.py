251ca8c54120573966f3b7ead77706f3b983532f
priyanka-chaurasia <Priyankachrs130899@gmail.com>
2020-05-03 13:54:49 +0530

    Users added sucessfully

---
+++ djangoweb1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\priya\Django-Project1\djangoweb1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ djangoweb1/src/mysite/urls.py
@@ -0,0 +1,44 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from mysite import settings
+from student_management_app import views , HodViews
+
+
+urlpatterns = [
+    path('demo',views.ShowDemo,name="showdemo"),
+    path('admin/', admin.site.urls),
+    path('login',views.ShowLogin,name="showlogin"),
+    path('get_user_details', views.GetUserDetails ,name="getuserdetails"),
+    path('logout', views.logout_user,name="logout"),
+    path('doLogin',views.doLogin,name="do_login"),
+    path('admin_home',HodViews.admin_home,name="admin_home"),
+    path('add_staff',HodViews.add_staff,name="add_staff"),
+    path('add_staff_save',HodViews.add_staff_save,name="add_staff_save"),
+    path('add_course', HodViews.add_course,name="add_course"),
+    path('add_course_save', HodViews.add_course_save,name="add_course_save"),
+    path('add_student', HodViews.add_student,name="add_student"),
+    path('add_student_save', HodViews.add_student_save,name="add_student_save"),
+    path('add_subject', HodViews.add_subject,name="add_subject"),
+    path('add_subject_save', HodViews.add_subject_save,name="add_subject_save"),
+    path('manage_staff', HodViews.manage_staff,name="manage_staff"),
+    path('manage_student', HodViews.manage_student,name="manage_student"),
+    path('manage_course', HodViews.manage_course,name="manage_course"),
+    path('manage_subject', HodViews.manage_subject,name="manage_subject"),
+
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
